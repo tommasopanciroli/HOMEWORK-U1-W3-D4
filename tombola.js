35637cfb5156ffc17034bac3fdf99660418b6fc5
tommaso.panciroli <tommypanci@gmail.com>
2024-10-17 15:47:54 +0200

    adds first files of the exercise

---
+++ tombola.js
@@ -0,0 +1,20 @@
+const numberArray = []
+for (let i = 1; i < 77; i++) {
+  numberArray.push(i)
+}
+
+const createNumberCells = function () {
+  const cellsSection = document.getElementById('cells')
+  const numberBingo = numberArray
+  cellsSection.innerHTML = ''
+
+  for (let i = 1; i < 77; i++) {
+    const cell = document.createElement('div')
+    const numberCell = document.createElement('h3')
+    numberCell.innerText = i
+    cell.appendChild(numberCell)
+    cellsSection.appendChild(cell)
+    cell
+}
+
+createNumberCells()
